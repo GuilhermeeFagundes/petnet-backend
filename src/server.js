e9db68318c43a0de1acf8efbea938909ed1e91eb
GuilhermeFaag <guilhermefag2@outlook.com>
2026-03-28 23:55:39 -0300

    feat: adicionar origem do Netlify no CORS

--- src/server.js
+++ src/server.js
@@ -7,7 +7,10 @@ import routes from './routes/index.js'; // Importa as rotas (e não o controller
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://petnet-pi.netlify.app'],
+  credentials: true
+}));
 app.use(cookieParser()); // Habilita leitura de cookies (req.cookies)
 
 // Rota de verificação (Health Check)
